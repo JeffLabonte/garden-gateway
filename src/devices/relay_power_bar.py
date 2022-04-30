a2b685617a52c74b8d11226a261b899a6bb8a36f
Jeff Labonte <grimsleepless@protonmail.com>
2022-04-30 00:23:59 -0400

    Add Relay status and add toggle

--- src/devices/relay_power_bar.py
+++ src/devices/relay_power_bar.py
@@ -1,12 +1,20 @@
+from enum import Enum
+
 import RPi.GPIO as GPIO
 
 POWER_RELAY_PIN = 17
+
+class RelayStatus(Enum):
+    STATUS_ON = 1
+    STATUS_OFF = 2
+
 
 
 class RelayPowerBar:
 
     def __init__(self, pin: int = POWER_RELAY_PIN):
         self._pin = pin
+        self._relay_status = RelayStatus.STATUS_OFF
         self._setup_board()
 
     def _setup_board(self):
@@ -24,3 +32,9 @@ class RelayPowerBar:
         Normal ON goes OFF and normal ON goes ON
         """
         GPIO.output(POWER_RELAY_PIN, GPIO.HIGH)
+
+    def toggle_relay(self):
+        if self._relay_status == RelayStatus.STATUS_ON:
+            self.set_relay_off()
+        elif self._relay_status == RelayStatus.STATUS_OFF:
+            self.set_relay_on()
