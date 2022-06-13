ccf987dd97fea4abd6a2e6eb301d901800726804
Jeff Labonte <grimsleepless@protonmail.com>
2022-06-12 23:49:08 -0400

    Update code

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,22 +1,18 @@
+struct CLIError {
+    pub name: String,
+    pub message: String,
+}
+
 struct CLIArgs {
     pub action: String,
     pub key: String,
     pub value: String,
+    pub errors: Vec<CLIError>,
 }
 
 impl CLIArgs {
-    pub fn new(action: String, key: String, value: String) -> Self {
-        match action.as_str() {
-            "config" => {
-                println!("This is config")
-            }
-            "run" => {
-                println!("Do nothing")
-            }
-            _ => panic!("Not supposed to happen"),
-        }
-
-        CLIArgs { action, key, value }
+    pub fn is_valid(&mut self) -> bool {
+        false
     }
 }
 
@@ -27,11 +23,17 @@ mod tests {
     use crate::cli::CLIArgs;
 
     #[test]
-    fn cli_args__new__action_config() {
+    fn cli_args_new_action_config() {
         let config_action = String::from("config");
         let empty_string = String::from("");
-        let args = CLIArgs::new(config_action, empty_string.clone(), empty_string.clone());
+        let mut args = CLIArgs {
+            action: config_action,
+            key: empty_string.clone(),
+            value: empty_string.clone(),
+            errors: vec![],
+        };
+        let _is_valid = args.is_valid();
 
-        assert_eq!(args.action, String::from("config"));
+        assert_eq!(_is_valid, true);
     }
 }
