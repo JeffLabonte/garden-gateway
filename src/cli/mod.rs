0616bdd1ed28f103d580518ea058bbb41168d24f
Jeff Labonte <grimsleepless@protonmail.com>
2022-06-19 14:29:25 -0400

    Add subaction to CLI

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -5,6 +5,9 @@ pub struct CLIArgs {
     #[clap(short, required = true)]
     pub action: String,
 
+    #[clap(short, required = false, default_value_t = String::from(""))]
+    pub sub_action: String,
+
     #[clap(short, long, value_parser, default_value_t = String::from(""))]
     pub key: String,
 
@@ -14,7 +17,12 @@ pub struct CLIArgs {
 
 fn validate_action(args: CLIArgs) -> CLIArgs {
     match args.action.as_str() {
-        "config" => args,
+        "config" => match args.sub_action.as_str() {
+            "list" => args,
+            "set" => args,
+            "remove" => args,
+            _ => panic!("You need to "),
+        },
         "run" => args,
         _ => panic!("Only config and run action are allowed"),
     }
