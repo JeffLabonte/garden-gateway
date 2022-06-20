0240059bd3e3986956ea02673d440236642e39f3
Jeff Labonte <grimsleepless@protonmail.com>
2022-06-20 00:38:17 -0400

    Update main to use Context instead of variables

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,24 @@ extern crate diesel;
 extern crate dotenv;
 
 mod cli;
+pub mod context;
 mod database;
 mod devices;
+pub mod models;
+pub mod schema;
 
+use cli::actions::run_action;
 use cli::get_cli_args;
 use database::establish_connection;
 use devices::RelayPowerBar;
 
+use crate::context::Context;
+
 fn main() {
-    let database = establish_connection();
-    let arguments = get_cli_args().expect("Invalid Arguments! Please use --help");
-    print!("Action: {}", arguments.action);
+    let context = Context {
+        database: establish_connection(),
+        arguments: get_cli_args().expect("Invalid Arguments! Please use --help"),
+    };
+    let is_success = run_action(context);
+    println!("{}", is_success);
 }
