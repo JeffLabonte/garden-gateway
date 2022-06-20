9f85a98a268d30df802c6c695f42bbeb4a7891a5
Jeff Labonte <grimsleepless@protonmail.com>
2022-06-20 01:02:23 -0400

    Change Error handling to make it more convenient

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,26 @@ mod devices;
 pub mod models;
 pub mod schema;
 
+use std::process;
+
 use cli::actions::run_action;
 use cli::get_cli_args;
 use database::establish_connection;
 use devices::RelayPowerBar;
 
+use crate::cli::CLIArgs;
 use crate::context::Context;
 
 fn main() {
     let context = Context {
         database: establish_connection(),
-        arguments: get_cli_args().expect("Invalid Arguments! Please use --help"),
+        arguments: match get_cli_args() {
+            Ok(x) => x,
+            Err(e) => {
+                eprint!("{}. Please use --help", e);
+                process::exit(0x01)
+            }
+        },
     };
     let is_success = run_action(context);
     println!("{}", is_success);
