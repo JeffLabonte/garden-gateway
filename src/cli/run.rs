e7e4ac58b09076dd70517f5aacc48332b076253e
Jeff Labonte <grimsleepless@protonmail.com>
2022-06-27 19:54:33 -0400

    Start implementing Hardware

--- src/cli/run.rs
+++ src/cli/run.rs
@@ -1,10 +1,31 @@
+use crate::devices::RelayPowerBar;
 use crate::models::*;
 use diesel::prelude::*;
 
 pub fn run(database: SqliteConnection) -> bool {
-    let configurations = configurations
+    use crate::schema::configurations::dsl::configurations;
+
+    let configs = configurations
         .load::<Configuration>(&database)
         .expect("Error loading configurations");
+
+    let mut thread_handlers: Vec<&str> = Vec::new();
+    for config in configs {
+        let bcm_pin: u8 = config.bcm_pin as u8;
+
+        let is_success = match config.sensor_name.as_str() {
+            "relay_bar" => {
+                let relay_power_bar = RelayPowerBar::new(bcm_pin);
+                thread_handlers.push("str");
+                true
+            }
+            _ => false,
+        };
+
+        if !is_success {
+            return is_success;
+        }
+    }
 
     true
 }
