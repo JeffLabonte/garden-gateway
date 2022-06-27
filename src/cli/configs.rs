0524065a5b016076e4aed7e2d4db73c23277ec4d
Jeff Labonte <grimsleepless@protonmail.com>
2022-06-26 22:54:57 -0400

    Create config,rs file

---
+++ src/cli/configs.rs
@@ -0,0 +1,21 @@
+use crate::models::*;
+use diesel::prelude::*;
+
+pub fn list_configs(database: SqliteConnection) -> bool {
+    use crate::schema::configurations::dsl::configurations;
+    let results = configurations
+        .load::<Configuration>(&database)
+        .expect("Error loading configurations");
+
+    println!("Displaying {} configs", results.len());
+
+    println!("Configurations:\n");
+    for config in results {
+        println!("************************\n");
+        println!("Sensor Name: {}", config.sensor_name);
+        println!("Pin Number Configured: {}\n", config.bcm_pin);
+        println!("************************\n");
+    }
+
+    true
+}
