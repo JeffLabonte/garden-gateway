e9f09774956e6293fec63e219bf1b0bb22e2fa1c
Jeff Labonte <grimsleepless@protonmail.com>
2022-06-29 11:36:09 -0400

    Create model for Schedule

--- src/models.rs
+++ src/models.rs
@@ -6,3 +6,11 @@ pub struct Configuration {
     pub sensor_name: String,
     pub bcm_pin: i32,
 }
+
+#[derive(Queryable, Insertable)]
+pub struct Schedule {
+    pub id: i32,
+    pub action: String,
+    pub cron_string: String,
+    pub configuration_id: i32,
+}
