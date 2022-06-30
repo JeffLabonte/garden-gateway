c2f3e4f6ff0453a0a37621804f5e97bf98c892c2
Jeff Labonte <grimsleepless@protonmail.com>
2022-06-29 23:09:36 -0400

    Create function to get_struct_from_configuration and add_job_to_scheduler

--- src/cli/run.rs
+++ src/cli/run.rs
@@ -3,6 +3,38 @@ use std::thread::{self, JoinHandle};
 use crate::devices::RelayPowerBar;
 use crate::models::*;
 use diesel::prelude::*;
+use tokio_cron_scheduler::{Job, JobScheduler, JobToRun};
+
+fn get_struct_from_configuration(configuration: Configuration) -> Box<dyn Devices> {
+    Box::new(RelayPowerBar::new(configuration.bcm_pin as u8))
+}
+
+fn add_job_to_scheduler(
+    database: &SqliteConnection,
+    scheduler: &JobScheduler,
+    configuration: Configuration,
+) -> () {
+    use crate::schema::schedules::dsl::{configuration_id, schedules};
+
+    let config_id = configuration.id;
+    let device = get_struct_from_configuration(configuration);
+
+    let results = schedules
+        .filter(configuration_id.eq(config_id))
+        .load::<Schedule>(database)
+        .expect("Error loading schedules");
+
+    for schedule in results {
+        match device.is_what_type().as_str() {
+            "RelayPowerBar" => match schedule.action.as_str() {
+                "turn_on" => (),
+                "turn_off" => (),
+                _ => (),
+            },
+            _ => (),
+        }
+    }
+}
 
 pub fn run(database: SqliteConnection) -> bool {
     use crate::schema::configurations::dsl::configurations;
