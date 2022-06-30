50a33525809e69c1aaf897ec1504a6e6665b81cd
Jeff Labonte <grimsleepless@protonmail.com>
2022-06-29 23:10:17 -0400

    update run function to use add_job_to_scheduler

--- src/cli/run.rs
+++ src/cli/run.rs
@@ -37,27 +37,14 @@ fn add_job_to_scheduler(
 pub fn run(database: SqliteConnection) -> bool {
     use crate::schema::configurations::dsl::configurations;
 
+    let scheduler = JobScheduler::new();
     let configs = configurations
         .load::<Configuration>(&database)
         .expect("Error loading configurations");
 
-    let mut thread_handlers: Vec<JoinHandle<()>> = Vec::new();
+    let mut scheduler = scheduler.unwrap();
     for config in configs {
-        let bcm_pin: u8 = config.bcm_pin as u8;
-
-        let is_success = match config.sensor_name.as_str() {
-            "relay_bar" => {
-                let relay_power_bar = RelayPowerBar::new(bcm_pin);
-                let handle = thread::spawn(move || {});
-                thread_handlers.push(handle);
-                true
-            }
-            _ => false,
-        };
-
-        if !is_success {
-            return is_success;
-        }
+        add_job_to_scheduler(&database, &scheduler, config);
     }
 
     true
