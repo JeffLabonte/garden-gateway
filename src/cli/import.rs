49d7965af94e30692f5cc0783e957aaa82855ce8
Jeff Labonte <grimsleepless@protonmail.com>
2022-07-07 02:51:33 -0400

    Adding struct and code to read JSON file and deserialize it to a ImportedSchedule

--- src/cli/import.rs
+++ src/cli/import.rs
@@ -1,5 +1,17 @@
+use std::fs::File;
 use std::path::PathBuf;
 
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct ImportedSchedule {
+    pub cron_string: String,
+    pub action: String,
+    pub configuration_id: i32,
+}
+
 pub fn import_schedule_from_json(file: PathBuf) -> bool {
+    let json_file = File::open(file).expect("No file there");
+    let schedules: Vec<ImportedSchedule> = serde_json::from_reader(json_file).expect("It failed");
     false
 }
