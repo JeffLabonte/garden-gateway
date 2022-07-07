d16f84f9f7422db3b8361dc7c71143f7b8269981
Jeff Labonte <grimsleepless@protonmail.com>
2022-07-07 16:03:03 -0400

    Create test block

--- src/cli/import.rs
+++ src/cli/import.rs
@@ -10,14 +10,26 @@ struct ImportedSchedule {
     pub configuration_id: i32,
 }
 
-pub fn import_schedule_from_json(file: PathBuf) -> bool {
+fn read_json_schedule(file: PathBuf) -> Vec<ImportedSchedule> {
     let json_file = match File::open(file) {
         Ok(f) => f,
         Err(e) => panic!("Unable to read file: {}", e),
     };
-    let schedules: Vec<ImportedSchedule> = match serde_json::from_reader(json_file) {
+    match serde_json::from_reader(json_file) {
         Ok(v) => v,
         Err(e) => panic!("Unable to deserialize JSON file: {}", e),
-    };
+    }
+}
+
+pub fn import_schedule_from_json(file: PathBuf) -> bool {
+    let imported_schedules = read_json_schedule(file);
     false
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn import_schedules_must_be_unique() {}
 }
