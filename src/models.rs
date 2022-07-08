2e138b97d40f13128705dd16011a91504757bde1
Jeff Labonte <grimsleepless@protonmail.com>
2022-07-08 03:31:48 -0400

    Add Insertable struct

--- src/models.rs
+++ src/models.rs
@@ -1,15 +1,30 @@
 use crate::schema::{configurations, schedules};
 
-#[derive(Queryable, Insertable)]
+#[derive(Queryable)]
 pub struct Configuration {
     pub id: i32,
     pub sensor_name: String,
     pub bcm_pin: i32,
 }
 
-#[derive(Queryable, Insertable)]
+#[derive(Insertable)]
+#[table_name="configurations"]
+pub struct NewConfiguration {
+    pub sensor_name: String,
+    pub bcm_pin: i32,
+}
+
+#[derive(Queryable)]
 pub struct Schedule {
     pub id: i32,
+    pub action: String,
+    pub cron_string: String,
+    pub configuration_id: i32,
+}
+
+#[derive(Insertable)]
+#[table_name="schedules"]
+pub struct NewSchedule {
     pub action: String,
     pub cron_string: String,
     pub configuration_id: i32,
