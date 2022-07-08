48c2d2f348d31595376fa114de2f9984efae25af
Jeff Labonte <grimsleepless@protonmail.com>
2022-07-07 22:58:24 -0400

    Add test for is_input_unique, add is_unique_from_db

--- src/cli/import.rs
+++ src/cli/import.rs
@@ -31,12 +31,51 @@ fn is_input_unique(schedules: Vec<ImportedSchedule>) -> bool {
     return original_schedules.len() == unique_schedules.len();
 }
 
-fn validate_input(schedules: Vec<ImportedSchedule>) -> bool {
-    is_input_unique(schedules)
+fn is_unique_with_db(database: SqliteConnection, imported_schedules: Vec<ImportedSchedule>) -> bool {
+    use crate::schema::schedules::dsl::{schedules, cron_string, configuration_id, action};
+
+    for imported_schedule in imported_schedules {
+        let db_schedules = schedules
+            .filter(
+                cron_string.eq(imported_schedule.cron_string),
+            )
+            .filter(
+                configuration_id.eq(imported_schedule.configuration_id),
+            )
+            .filter(
+                action.eq(imported_schedule.action),
+            )
+            .load::<Schedule>(&database)
+            .expect("Error Loading Configurations");
+
+        if db_schedules.is_empty() == false {
+            return false;
+        }
+    }
+
+    true
 }
 
-pub fn import_schedule_from_json(file: PathBuf) -> bool {
+fn validate_input(database: SqliteConnection, schedules: Vec<ImportedSchedule>) -> bool {
+    if ! is_input_unique(schedules.clone()) {
+        return false;
+    }
+    
+    if ! is_unique_with_db(database, schedules.clone()) {
+        return false;
+    }
+
+    true
+}
+
+pub fn import_schedule_from_json(database: SqliteConnection, file: PathBuf) -> bool {
     let imported_schedules = read_json_schedule(file);
+    let is_valid = validate_input(database, imported_schedules);
+
+    if is_valid == false {
+        return is_valid;
+    }
+
     false
 }
 
@@ -44,6 +83,7 @@ pub fn import_schedule_from_json(file: PathBuf) -> bool {
 mod tests {
     use test_case::test_case;
     use super::*;
+    use crate::database::establish_connection;
 
     fn generate_imported_schedule(size: u32) -> Vec<ImportedSchedule> {
         let mut imported_schedules: Vec<ImportedSchedule> = Vec::new();
@@ -58,7 +98,7 @@ mod tests {
         return imported_schedules;
     }
     
-    #[test_case(true, true ; "When unique expect valid")]
+    #[test_case(true, true; "When unique expect valid")]
     #[test_case(false, false ; "When not unique expect invalid")]
     fn import_schedules_must_be_unique(is_unique: bool, is_schedules_valid: bool) {
         let mut imported_schedules = generate_imported_schedule(2);
@@ -67,7 +107,9 @@ mod tests {
             imported_schedules[0].configuration_id = 2;
         }
 
-        let result: bool = validate_input(imported_schedules);
+        let database = establish_connection();
+
+        let result: bool = validate_input(database, imported_schedules);
         assert_eq!(result, is_schedules_valid);
     }
 }
