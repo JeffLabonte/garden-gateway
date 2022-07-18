28a6a2a88ebfef32bf0a85008f5731eb76aafd68
Jeff Labonte <grimsleepless@protonmail.com>
2022-07-17 23:17:09 -0400

    Add WaterPump alone

--- src/devices.rs
+++ src/devices.rs
@@ -1,7 +1,27 @@
 use rust_gpiozero::output_devices::OutputDevice;
 
+const TURN_ON_STRING: &str = "TURN ON";
+const TURN_OFF_STRING: &str = "TURN OFF";
+
+const RELAY_POWER_BAR: &str = "RelayPowerBar";
+const WATER_PUMP: &str = "Water Pump";
+
+fn println_now(action: &str, board: &str) {
+    let now = chrono::Local::now();
+    println!(
+        "{} - Running: {} pin to {}",
+        now.format("%b %-d, %-I:%M:%s"),
+        action,
+        board,
+    );
+}
+
 pub struct RelayPowerBar {
     relay_power_pin: OutputDevice,
+}
+
+pub struct WaterPump {
+    gpio_pin: OutputDevice,
 }
 
 impl RelayPowerBar {
@@ -11,14 +31,29 @@ impl RelayPowerBar {
     }
 
     pub fn turn_on(&mut self) {
-        let now = chrono::Local::now();
-        println!("{} - Running: Turning ON pin to RelayPowerBar", now.format("%b %-d, %-I:%M:%s").to_string());
+        println_now(TURN_ON_STRING, RELAY_POWER_BAR);
         self.relay_power_pin.off();
     }
 
     pub fn turn_off(&mut self) {
-        let now = chrono::Local::now();
-        println!("{} - Running: Turning OFF pin to RelayPowerBar", now.format("%b %-d, %-I:%M:%s").to_string());
+        println_now(TURN_OFF_STRING, RELAY_POWER_BAR);
         self.relay_power_pin.on();
+    }
+}
+
+impl WaterPump {
+    pub fn new(gpio_pin: u8) -> WaterPump {
+        let gpio_pin = OutputDevice::new(gpio_pin);
+        WaterPump { gpio_pin }
+    }
+
+    pub fn turn_on(&mut self) {
+        println_now(TURN_ON_STRING, WATER_PUMP);
+        self.gpio_pin.on();
+    }
+
+    pub fn turn_off(&mut self) {
+        println_now(TURN_OFF_STRING, WATER_PUMP);
+        self.gpio_pin.off();
     }
 }
