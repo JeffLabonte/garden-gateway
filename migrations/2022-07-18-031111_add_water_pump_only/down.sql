72f53b5e6c3172dc8062082bd7bea6d93204846b
Jeff Labonte <grimsleepless@protonmail.com>
2022-07-17 23:16:05 -0400

    Add migration to add water_pump alone

---
+++ migrations/2022-07-18-031111_add_water_pump_only/down.sql
@@ -0,0 +1,3 @@
+-- This file should undo anything in `up.sql`
+DELETE FROM configurations
+WHERE sensor_name = 'water_pump';
---
+++ migrations/2022-07-18-031111_add_water_pump_only/up.sql
@@ -0,0 +1,3 @@
+-- Your SQL goes here
+INSERT OR IGNORE INTO configurations ( sensor_name, bcm_pin)
+VALUES ('water_pump', 4);
