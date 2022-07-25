adc47fe8c3db0f1b34c618f675c2222ac4ec2a31
Jeff Labonte <grimsleepless@protonmail.com>
2022-07-25 00:38:36 +0000

    Add extra devices to complete board implementation

--- src/devices.rs
+++ src/devices.rs
@@ -1,18 +1,28 @@
-use rust_gpiozero::output_devices::OutputDevice;
+use rust_gpiozero::{output_devices::OutputDevice, InputDevice};
 use crate::helpers::println_now;
 
 const TURN_ON_STRING: &str = "TURN ON";
 const TURN_OFF_STRING: &str = "TURN OFF";
 
 const RELAY_POWER_BAR: &str = "RelayPowerBar";
-const WATER_PUMP: &str = "Water Pump";
+const WATER_PUMP: &str = "WaterPump";
+
 
 pub struct RelayPowerBar {
     relay_power_pin: OutputDevice,
 }
 
+pub struct WateringSystem {
+    water_pump: WaterPump,
+    water_detector: WaterDetector,
+}
+
+pub struct WaterDetector {
+    input_device: InputDevice,
+}
+
 pub struct WaterPump {
-    gpio_pin: OutputDevice,
+    gpio_device: OutputDevice,
 }
 
 impl RelayPowerBar {
@@ -34,8 +44,8 @@ impl RelayPowerBar {
 
 impl WaterPump {
     pub fn new(gpio_pin: u8) -> WaterPump {
-        let gpio_pin = OutputDevice::new(gpio_pin);
-        WaterPump { gpio_pin }
+        let gpio_device = OutputDevice::new(gpio_pin);
+        WaterPump { gpio_device }
     }
 
     pub fn turn_on(&mut self) {
@@ -46,5 +56,16 @@ impl WaterPump {
     pub fn turn_off(&mut self) {
         println_now(TURN_OFF_STRING, WATER_PUMP);
         self.gpio_pin.off();
+    }
+}
+
+impl WaterDetector {
+    pub fn new(bcm_pin: u8) -> WaterDetector {
+        let input_device: InputDevice = InputDevice::new(bcm_pin);
+        WaterDetector { input_device }
+    }
+
+    pub fn is_active(&mut self) -> bool{
+        self.input_device.is_active()
     }
 }
