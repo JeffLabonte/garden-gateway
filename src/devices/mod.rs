a9781e77f1605ddc8b35760462cc52214bf209ba
Jeff Labonte <grimsleepless@protonmail.com>
2022-07-31 21:25:11 +0000

    Remove implementation from mod.rs in devices

--- src/devices/mod.rs
+++ src/devices/mod.rs
@@ -3,7 +3,6 @@ pub mod watering_system;
 
 use crate::helpers::println_now;
 use rust_gpiozero::{output_devices::OutputDevice, InputDevice};
-use std::time::Duration;
 
 use self::constants::{WATER_PUMP, RELAY_POWER_BAR, TURN_ON_STRING, TURN_OFF_STRING};
 
@@ -26,50 +25,5 @@ impl RelayPowerBar {
     pub fn turn_off(&mut self) {
         println_now(TURN_OFF_STRING, RELAY_POWER_BAR);
         self.relay_power_device.on();
-    }
-}
-
-impl WateringSystem {
-    pub fn new(water_detector_pin: u8, water_pump_pin: u8) -> WateringSystem {
-        WateringSystem {
-            water_pump: WaterPump::new(water_pump_pin),
-            water_detector: WaterDetector::new(water_detector_pin),
-        }
-    }
-
-    pub fn water_until_filled(&mut self) {
-        while !self.water_detector.has_water() {
-            self.water_pump.turn_on();
-            std::thread::sleep(Duration::from_secs(1));
-            self.water_pump.turn_off();
-        }
-    }
-}
-
-impl WaterPump {
-    pub fn new(gpio_pin: u8) -> WaterPump {
-        let gpio_device = OutputDevice::new(gpio_pin);
-        WaterPump { gpio_device }
-    }
-
-    pub fn turn_on(&mut self) {
-        println_now(TURN_ON_STRING, WATER_PUMP);
-        self.gpio_device.on();
-    }
-
-    pub fn turn_off(&mut self) {
-        println_now(TURN_OFF_STRING, WATER_PUMP);
-        self.gpio_device.off();
-    }
-}
-
-impl WaterDetector {
-    pub fn new(bcm_pin: u8) -> WaterDetector {
-        let input_device: InputDevice = InputDevice::new(bcm_pin);
-        WaterDetector { input_device }
-    }
-
-    pub fn has_water(&mut self) -> bool {
-        self.input_device.is_active()
     }
 }
