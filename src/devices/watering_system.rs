17500a4f2e2865ad4aac86a14a681bb1ab75d6fa
Jeff Labonte <grimsleepless@protonmail.com>
2022-07-31 21:26:04 +0000

    Bring back implementation to watering_system.rs

--- src/devices/watering_system.rs
+++ src/devices/watering_system.rs
@@ -1,3 +1,11 @@
+use rust_gpiozero::{OutputDevice, InputDevice};
+
+use std::time::Duration;
+
+use crate::helpers::println_now;
+
+use super::constants::{TURN_OFF_STRING, TURN_ON_STRING, WATER_PUMP};
+
 pub struct WateringSystem {
     water_pump: WaterPump,
     water_detector: WaterDetector,
@@ -9,4 +17,49 @@ pub struct WaterDetector {
 
 pub struct WaterPump {
     gpio_device: OutputDevice,
+}
+
+impl WateringSystem {
+    pub fn new(water_detector_pin: u8, water_pump_pin: u8) -> WateringSystem {
+        WateringSystem {
+            water_pump: WaterPump::new(water_pump_pin),
+            water_detector: WaterDetector::new(water_detector_pin),
+        }
+    }
+
+    pub fn water_until_filled(&mut self) {
+        while !self.water_detector.has_water() {
+            self.water_pump.turn_on();
+            std::thread::sleep(Duration::from_secs(1));
+            self.water_pump.turn_off();
+        }
+    }
+}
+
+impl WaterPump {
+    pub fn new(gpio_pin: u8) -> WaterPump {
+        let gpio_device = OutputDevice::new(gpio_pin);
+        WaterPump { gpio_device }
+    }
+
+    pub fn turn_on(&mut self) {
+        println_now(TURN_ON_STRING, WATER_PUMP);
+        self.gpio_device.on();
+    }
+
+    pub fn turn_off(&mut self) {
+        println_now(TURN_OFF_STRING, WATER_PUMP);
+        self.gpio_device.off();
+    }
+}
+
+impl WaterDetector {
+    pub fn new(bcm_pin: u8) -> WaterDetector {
+        let input_device: InputDevice = InputDevice::new(bcm_pin);
+        WaterDetector { input_device }
+    }
+
+    pub fn has_water(&mut self) -> bool {
+        self.input_device.is_active()
+    }
 }
