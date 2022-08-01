c5e1ad56fee6b8903f1fa7017811de5b1caed8ba
Jeff Labonte <grimsleepless@protonmail.com>
2022-08-01 02:42:22 +0000

    Change the new function and remove unused variables

--- src/devices/mod.rs
+++ src/devices/mod.rs
@@ -2,13 +2,10 @@ mod constants;
 pub mod watering_system;
 pub mod relay_power;
 
-use crate::helpers::println_now;
-use rust_gpiozero::{output_devices::OutputDevice};
-
-use self::constants::{RELAY_POWER_BAR, TURN_ON_STRING, TURN_OFF_STRING};
+use std::collections::HashMap;
 
 trait Device {
-    fn new(bcm_pin: u8) -> Self;
+    fn new(sensor_pins: HashMap<String, u8>) -> Self;
     fn turn_on(&mut self);
     fn turn_off(&mut self);
 }
