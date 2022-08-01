b62243d5c968589cfa6aadb0199782b1ba5e698a
Jeff Labonte <grimsleepless@protonmail.com>
2022-08-01 02:49:11 +0000

    Change the String for &str

--- src/devices/mod.rs
+++ src/devices/mod.rs
@@ -5,7 +5,7 @@ pub mod relay_power;
 use std::collections::HashMap;
 
 trait Device {
-    fn new(sensor_pins: HashMap<String, u8>) -> Self;
+    fn new(sensor_pins: HashMap<&str, u8>) -> Self;
     fn turn_on(&mut self);
     fn turn_off(&mut self);
 }
