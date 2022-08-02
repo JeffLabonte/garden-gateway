32fe36b3b6d9b1c53085a89e0faaf446ba79708a
Jeff Labonte <grimsleepless@protonmail.com>
2022-08-02 02:47:26 +0000

    Remove configuration ID from CREATE TABLE

--- migrations/2022-06-28-020812_create_schedules/up.sql
+++ migrations/2022-06-28-020812_create_schedules/up.sql
@@ -2,6 +2,5 @@
 CREATE TABLE schedules (
     id INTEGER PRIMARY KEY NOT NULL,
     action TEXT NOT NULL,
-    cron_string TEXT NOT NULL,
-    configuration_id INTEGER NOT NULL,
+    cron_string TEXT NOT NULL
 )
