8996e826790e55057798db739d62a71648be83f2
Jeff Labonte <grimsleepless@protonmail.com>
2022-08-28 21:08:40 +0000

    Add schedule_configs to list

--- src/cli/schedules.rs
+++ src/cli/schedules.rs
@@ -3,6 +3,7 @@ use diesel::prelude::*;
 
 pub fn list_schedules(database: &SqliteConnection) -> bool {
     use crate::schema::configurations::dsl::configurations;
+    use crate::schema::schedule_configurations::dsl::{schedule_configurations, schedule_id};
     use crate::schema::schedules::dsl::schedules;
 
     let schedule_vec = schedules
@@ -12,8 +13,16 @@ pub fn list_schedules(database: &SqliteConnection) -> bool {
     for schedule in schedule_vec {
         println!("************************\n");
         println!("Schedules: {}", schedule.id);
+        let schedule_configs = schedule_configurations
+            .filter(schedule_id.eq(schedule.id))
+            .load::<ScheduleConfiguration>(database)
+            .expect("Unable to retrieve ScheduleConfigurations");
+
+        for schedule_config in schedule_configs {
+            println!("The Schedule Config: {}", schedule_config.id);
+        }
         println!("************************\n");
     }
 
-    return true;
+    true
 }
