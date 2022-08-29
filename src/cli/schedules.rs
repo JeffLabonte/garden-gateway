ea1f3ce75a5c41069c110d9b91e2fcb2990adc97
Jeff Labonte <grimsleepless@protonmail.com>
2022-08-29 02:18:31 +0000

    Complete work on list schedules with configs

--- src/cli/schedules.rs
+++ src/cli/schedules.rs
@@ -1,8 +1,8 @@
 use crate::models::*;
-use diesel::prelude::*;
+use diesel::{associations::HasTable, prelude::*};
 
 pub fn list_schedules(database: &SqliteConnection) -> bool {
-    use crate::schema::configurations::dsl::configurations;
+    use crate::schema::configurations::dsl::{bcm_pin, configurations, id, sensor_name};
     use crate::schema::schedule_configurations::dsl::{schedule_configurations, schedule_id};
     use crate::schema::schedules::dsl::schedules;
 
@@ -13,13 +13,18 @@ pub fn list_schedules(database: &SqliteConnection) -> bool {
     for schedule in schedule_vec {
         println!("************************\n");
         println!("Schedules: {}", schedule.id);
-        let schedule_configs = schedule_configurations
+        println!("Cron String in UTC : {}", schedule.cron_string);
+        let configs = configurations
+            .inner_join(schedule_configurations::table())
             .filter(schedule_id.eq(schedule.id))
-            .load::<ScheduleConfiguration>(database)
-            .expect("Unable to retrieve ScheduleConfigurations");
+            .select((bcm_pin, sensor_name, id))
+            .load::<Configuration>(database)
+            .expect("Unable to load schedules_configurations");
 
-        for schedule_config in schedule_configs {
-            println!("The Schedule Config: {}", schedule_config.id);
+        for conf in configs {
+            println!("Config ID: {}", conf.id);
+            println!("Pin: {}", conf.bcm_pin);
+            println!("Sensor Name: {}", conf.sensor_name);
         }
         println!("************************\n");
     }
