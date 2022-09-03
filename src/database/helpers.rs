52fe75de8e4a1280d733db4a55e6a4554dbe2911
Jeff Labonte <grimsleepless@protonmail.com>
2022-09-03 23:29:47 +0000

    Update test

--- src/database/helpers.rs
+++ src/database/helpers.rs
@@ -6,6 +6,23 @@ pub fn retrieve_schedules_from_config_id(config_id: u32) -> Vec<Schedule> {
 
 #[cfg(test)]
 mod tests {
+    use diesel::SqliteConnection;
+
+    use crate::{models::NewSchedule, schema::schedules};
+
+    fn create_base_data(database: &SqliteConnection) {
+        let configuraation_id: i32 = 1;
+        let default_schedule = NewSchedule {
+            action: "turn_off".to_string(),
+            cron_string: "* * * * *".to_string(),
+        };
+        let result = diesel::insert_or_ignore_into(schedules::table)
+            .values(default_schedule)
+            .execute(database)
+            .expect("Unable to insert Schedule");
+
+        
+    }
     #[test]
     fn test_retrieve_schedules_from_config_id() {}
 }
