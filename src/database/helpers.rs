42571dc2abf763572bbb2b230e85597415b4ecd8
Jeff Labonte <grimsleepless@protonmail.com>
2022-09-05 19:46:45 -0400

    Complete test helper to create base data

--- src/database/helpers.rs
+++ src/database/helpers.rs
@@ -11,22 +11,49 @@ mod tests {
     use diesel::SqliteConnection;
 
     use crate::diesel::RunQueryDsl;
+    use crate::models::{NewConfiguration, Configuration, NewScheduleConfiguration};
+    use crate::schema::{configurations, schedule_configurations};
     use crate::{models::NewSchedule, models::Schedule, schema::schedules};
 
     fn create_base_data(database: &SqliteConnection) {
-        let configuraation_id: i32 = 1;
         let default_schedule = NewSchedule {
             action: "turn_off".to_string(),
             cron_string: "* * * * *".to_string(),
         };
+        let default_configuration = NewConfiguration {
+            bcm_pin: 1,
+            sensor_name: "Test Sensor".to_string(),
+        };
+
         diesel::insert_or_ignore_into(schedules::table)
             .values(&default_schedule)
-            .execute(database);
+            .execute(database)
+            .expect("Unable to insert the new Schedule");
+
+        diesel::insert_or_ignore_into(configurations::table)
+            .values(&default_configuration)
+            .execute(database)
+            .expect("Unable to insert the new Configuration");
 
         let last_inserted_schedule = schedules::dsl::schedules
             .order_by(schedules::dsl::id.desc())
             .first::<Schedule>(database)
             .expect("Unable to retrieve the latest Schedule");
+
+        let last_inserted_config = configurations::dsl::configurations
+            .order_by(configurations::dsl::id.desc())
+            .first::<Configuration>(database)
+            .expect("Unable to retrieve the lastest Configuration");
+
+        let schedule_configuration =  NewScheduleConfiguration {
+            schedule_id: last_inserted_schedule.id,
+            configuration_id: last_inserted_config.id,
+        };
+
+        diesel::insert_or_ignore_into(schedule_configurations::table)
+            .values(schedule_configuration)
+            .execute(database)
+            .expect("Unable to insert the new Schedule Configuration");
     }
 
     #[test]
