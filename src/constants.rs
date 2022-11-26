937bc70763f648b44076a0ad5c8a52d74466b7ed
Jean-Francois Labonte <grimsleepless@protonmail.com>
2022-11-26 01:50:57 -0500

    Move constants variable into constant file

---
+++ src/constants.rs
@@ -0,0 +1,6 @@
+
+pub const WATER_PUMP_SENSOR_NAME: &str = "";
+pub const RELAY_POWER_SENSOR_NAME: &str = "relay_power";
+
+pub const TURN_ON_ACTION: &str = "turn_on";
+pub const TURN_OFF_ACTION: &str = "turn_off";
