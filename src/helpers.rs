e23fe93030890b17bd2ff8d0208c1db637860a84
Jean-Francois Labonte <grimsleepless@protonmail.com>
2022-11-26 01:52:12 -0500

    Bring old code and seperate action into function

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,3 +1,16 @@
+use std::collections::HashMap;
+
+use tokio_cron_scheduler::{JobScheduler, Job};
+use uuid::Uuid;
+
+use crate::{
+    constants::{TURN_OFF_ACTION, TURN_ON_ACTION},
+    database::helpers::{get_all_configurations, retrieve_schedules_from_config_id},
+    devices::relay_power::RelayPowerBar,
+    devices::Device,
+    models::Configuration,
+};
+
 const DATETIME_FORMAT: &str = "%b %-d, %-I:%M:%s";
 
 pub fn println_now(action: &str, board: &str) {
@@ -10,4 +23,55 @@ pub fn println_now(action: &str, board: &str) {
         board,
     );
     print!("Time in UTC: {}", now_utc.format(DATETIME_FORMAT));
+}
+
+fn add_job_to_scheduler(scheduler: &JobScheduler, configuration: Configuration) -> Vec<Uuid> {
+    // TODO Implement with new tables
+    let config_id = configuration.id;
+    let results = retrieve_schedules_from_config_id(config_id);
+
+    let mut job_ids = Vec::new();
+    for sched in results {
+        let cron_schedule_str = sched.cron_string.as_str();
+        println!("This schedule will run at {}", cron_schedule_str);
+
+        match sched.action.as_str() {
+            TURN_ON_ACTION => {
+                println!("Adding turn_on for configuration {}", 0);
+                let job = Job::new(cron_schedule_str, move |_, _| {
+                    let pins = HashMap::from([("relay_power_pin", configuration.bcm_pin as u8)]);
+                    let mut device = RelayPowerBar::new(pins);
+                    device.turn_on();
+                })
+                .unwrap();
+                job_ids.push(scheduler.add(job).unwrap());
+            }
+            TURN_OFF_ACTION => {
+                println!("Adding turn_off for configuration {}", 0);
+                let job = Job::new(cron_schedule_str, move |_, _| {
+                    let pins = HashMap::from([("relay_power_pin", configuration.bcm_pin as u8)]);
+                    let mut device = RelayPowerBar::new(pins);
+                    device.turn_off();
+                })
+                .unwrap();
+                job_ids.push(scheduler.add(job).unwrap());
+            }
+            _ => panic!("Action not implemented"),
+        };
+    }
+
+    job_ids
+}
+
+pub fn populate_job_ids(scheduler: &JobScheduler) -> Vec<Uuid> {
+    let mut job_ids: Vec<Uuid> = Vec::new();
+    let configurations: Vec<Configuration> = get_all_configurations();
+
+    for config in configurations {
+        for job_id in add_job_to_scheduler(&scheduler, config) {
+            job_ids.push(job_id);
+        }
+    }
+
+    job_ids
 }
