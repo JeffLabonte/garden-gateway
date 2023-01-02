cdf99d09220e1e0752852201bf2d3334d29cab66
Jean-Francois Labonte <grimsleepless@protonmail.com>
2023-01-02 01:13:39 -0500

    Update establish_connection to accept DB connection

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -6,7 +6,11 @@ pub mod helpers;
 
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATBASE_URL must be set");
+    let mut database_url = env::var("DATABASE_URL").expect("DATBASE_URL must be set");
+    if cfg!(test) {
+        database_url = ":memory".to_string();
+    }
+
     let mut connection = SqliteConnection::establish(&database_url);
 
     while connection.is_err() {
