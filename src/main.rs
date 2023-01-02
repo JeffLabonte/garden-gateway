87ad20a63a1f4bce897ec079293b2b634bdcada2
Jean-Francois Labonte <grimsleepless@protonmail.com>
2023-01-01 19:22:51 -0500

    Update the main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,7 @@
-#[macro_use]
-extern crate diesel;
-extern crate dotenv;
-
-mod cli;
-pub mod context;
-mod database;
-mod devices;
-mod helpers;
-mod models;
-mod schema;
-mod constants;
-
-use lazy_static::lazy_static;
-use std::sync::Mutex;
-
-use cli::actions::run_action;
-use cli::get_cli_args;
-use database::establish_connection;
-use diesel::SqliteConnection;
-
-use crate::context::Context;
-
-lazy_static! {
-    pub static ref DATABASE_CONNECTION: Mutex<SqliteConnection> =
-        Mutex::new(establish_connection());
-}
+use gateway::{
+    cli::{actions::run_action, get_cli_args},
+    context::Context,
+};
 
 fn main() {
     let context = Context {
