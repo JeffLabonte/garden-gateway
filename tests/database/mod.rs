01ad7aa2ecb6b3c5114bf53b21086f0d579c3f06
Jean-Francois Labonte <grimsleepless@protonmail.com>
2023-01-06 23:36:06 -0500

    Add missing connection argument

--- tests/database/mod.rs
+++ tests/database/mod.rs
@@ -1,6 +1,8 @@
 use diesel::Connection;
 use diesel::{result::Error, SqliteConnection};
-use gateway::database::helpers::{get_all_configurations, get_database_connection};
+use gateway::database::helpers::{
+    get_all_configurations, get_configurations_by_schedule_id, get_database_connection,
+};
 
 use crate::common::{create_configuration, create_schedule, link_configuration_to_schedule};
 
@@ -62,14 +64,17 @@ fn given_get_all_configurations_when_has_five_configurations_then_should_return_
 fn given_get_confgurations_from_schedule_id_when_two_configuration_linked_config_then_should_return_two(
 ) {
     get_database_connection().test_transaction::<_, Error, _>(|connection| {
-        let sensor_f_configuration = create_configuration("Sensor F".to_string(), 10, connection);
-        let sensor_g_configuration = create_configuration("Sensor G".to_string(), 11, connection);
+        let sensor_a_configuration = create_configuration("Sensor A".to_string(), 10, connection);
+        let sensor_b_configuration = create_configuration("Sensor B".to_string(), 11, connection);
 
         let schedule =
             create_schedule("0 0 0 * * *".to_string(), "turn_on".to_string(), connection);
 
-        link_configuration_to_schedule(schedule.id, sensor_f_configuration.id, connection);
-        link_configuration_to_schedule(schedule.id, sensor_g_configuration.id, connection);
+        link_configuration_to_schedule(schedule.id, sensor_a_configuration.id, connection);
+        link_configuration_to_schedule(schedule.id, sensor_b_configuration.id, connection);
+
+        let configurations = get_configurations_by_schedule_id(schedule.id, connection);
+        assert_eq!(configurations.len(), 2);
 
         Ok(())
     });
