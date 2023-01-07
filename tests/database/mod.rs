eb60add3749c694076a981881faac39b41ae466e
Jean-Francois Labonte <grimsleepless@protonmail.com>
2023-01-06 23:27:32 -0500

    Uncomment second test and fix warning

--- tests/database/mod.rs
+++ tests/database/mod.rs
@@ -5,7 +5,7 @@ use gateway::database::helpers::{get_all_configurations, get_database_connection
 use crate::common::{create_configuration, create_schedule, link_configuration_to_schedule};
 
 #[test]
-fn given_get_all_configurations__when_has_five_configurations__then_should_return_five_configs() {
+fn given_get_all_configurations_when_has_five_configurations_then_should_return_five_configs() {
     get_database_connection().test_transaction::<_, Error, _>(|connection| {
         let configurations = get_all_configurations(connection);
         assert_eq!(configurations.len(), 2);
@@ -58,14 +58,19 @@ fn given_get_all_configurations__when_has_five_configurations__then_should_retur
     })
 }
 
-//#[test]
-//fn given_get_confgurations_from_schedule_id__when_two_configuration_linked_config__then_should_return_two(
-//) {
-//    let sensor_f_configuration = create_configuration("Sensor F".to_string(), 10);
-//    let sensor_g_configuration = create_configuration("Sensor G".to_string(), 11);
-//
-//    let schedule = create_schedule("0 0 0 * * *".to_string(), "turn_on".to_string());
-//
-//    link_configuration_to_schedule(schedule.id, sensor_f_configuration.id);
-//    link_configuration_to_schedule(schedule.id, sensor_g_configuration.id);
-//}
+#[test]
+fn given_get_confgurations_from_schedule_id_when_two_configuration_linked_config_then_should_return_two(
+) {
+    get_database_connection().test_transaction::<_, Error, _>(|connection| {
+        let sensor_f_configuration = create_configuration("Sensor F".to_string(), 10, connection);
+        let sensor_g_configuration = create_configuration("Sensor G".to_string(), 11, connection);
+
+        let schedule =
+            create_schedule("0 0 0 * * *".to_string(), "turn_on".to_string(), connection);
+
+        link_configuration_to_schedule(schedule.id, sensor_f_configuration.id, connection);
+        link_configuration_to_schedule(schedule.id, sensor_g_configuration.id, connection);
+
+        Ok(())
+    });
+}
