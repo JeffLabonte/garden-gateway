5aad804230ee148ab2cb1647bad98f0ea4fe3e2a
Jean-Francois Labonte <grimsleepless@protonmail.com>
2023-01-08 01:43:14 -0500

    Simplify CI + format fixes

--- shell.nix
+++ shell.nix
@@ -2,10 +2,10 @@ let
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
   rust = (
-  nixpkgs.rustChannelOf { 
-    channel = "stable";
-  }).rust.override {
-    extensions = [ 
+    nixpkgs.rustChannelOf {
+      channel = "stable";
+    }).rust.override {
+    extensions = [
       "rust-src"
       "rust-analysis"
       "rustfmt-preview"
@@ -13,30 +13,31 @@ let
     ];
   };
 in
-  with nixpkgs;
-  stdenv.mkDerivation {
-    name = "Garden Gateway";
-    buildInputs = [
-      rustup
-        rust
-        rust-analyzer
-        pkgconfig 
-        openssl 
-        sqlite
-        rustc
-        gnumake
-        git
-        git-lfs
-        clippy
-        rustfmt
-     ];
-    packages = with nixpkgs; [
-      clippy
-      python310
-      cargo
-      ripgrep
-      nerdfonts
-    ];
-    # Set Environment Variables
-    RUST_BACKTRACE = 1;
-  }
+with nixpkgs;
+stdenv.mkDerivation {
+  name = "Garden Gateway";
+  buildInputs = [
+    rustup
+    rust
+    rust-analyzer
+    pkgconfig
+    openssl
+    sqlite
+    rustc
+    gnumake
+    git
+    git-lfs
+    clippy
+    rustfmt
+  ];
+  packages = with nixpkgs; [
+    clippy
+    python310
+    cargo
+    cargo-tarpaulin
+    ripgrep
+    nerdfonts
+  ];
+  # Set Environment Variables
+  RUST_BACKTRACE = 1;
+}
