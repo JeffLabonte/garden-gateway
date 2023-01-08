3e46fb5f63af49425e7fe9f6b909fc157be82496
Jean-Francois Labonte <grimsleepless@protonmail.com>
2023-01-07 20:48:29 -0500

    Fix helper query to get configuratins

--- src/database/helpers.rs
+++ src/database/helpers.rs
@@ -32,27 +32,15 @@ pub fn get_configurations_by_schedule_id(
     schedule_id: i32,
     database_connection: &mut SqliteConnection,
 ) -> Vec<Configuration> {
-    use crate::schema::configurations::dsl::id;
-
-    let schedule_config_vec = schedule_configurations::table
+    configurations::table
+        .inner_join(
+            schedule_configurations::table
+                .on(schedule_configurations::dsl::configuration_id.eq(configurations::dsl::id)),
+        )
         .filter(schedule_configurations::dsl::schedule_id.eq(schedule_id))
-        .load::<ScheduleConfiguration>(database_connection)
-        .expect("Error Loading Schedule Configurations");
-
-    println!("Schedule Configurations: {:?}", schedule_config_vec);
-
-    let config_ids = schedule_config_vec
-        .iter()
-        .map(|schedule_config| schedule_config.configuration_id)
-        .collect::<Vec<i32>>();
-
-    println!("Config ids: {:?}", config_ids);
-    let configs = configurations::table
-        .filter(id.eq_any(config_ids))
+        .select(configurations::all_columns)
         .load::<Configuration>(database_connection)
-        .expect("Error Loading Configurations");
-
-    configs
+        .expect("Error Loading Configurations")
 }
 
 pub fn get_database_connection() -> std::sync::MutexGuard<'static, SqliteConnection> {
