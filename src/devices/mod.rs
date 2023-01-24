7f30f0765dd42aa9f2bfd6971a4a859bcf48b15d
Jean-Francois Labonte <grimsleepless@protonmail.com>
2023-01-23 22:41:26 -0500

    Fix match for relay bar sensor name

--- src/devices/mod.rs
+++ src/devices/mod.rs
@@ -18,7 +18,7 @@ pub fn build_device(
     sensor_pins: HashMap<String, u8>,
 ) -> Box<dyn Device + Send + Sync> {
     match sensor_name {
-        constants::RELAY_POWER_BAR => Box::new(RelayPowerBar::new(sensor_pins)),
+        RELAY_BAR_SENSOR_NAME => Box::new(RelayPowerBar::new(sensor_pins)),
         WATER_DETECTOR_SENSOR_NAME | WATER_PUMP_SENSOR_NAME => {
             Box::new(WateringSystem::new(sensor_pins))
         }
